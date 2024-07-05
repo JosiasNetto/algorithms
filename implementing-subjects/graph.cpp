798eca1a4326957c8e5005753894287bb7df6765
Josias Netto <josiascaldasnetto08@gmail.com>
2024-07-05 13:09:52 -0300

    Comented graphs implement

--- implementing-subjects/graph.cpp
+++ implementing-subjects/graph.cpp
@@ -14,33 +14,37 @@ typedef struct graph{
 //Func that create a graph of n vertices
 Graph* create_graph(int n){
   Graph* newGraph = (Graph*) malloc(sizeof(Graph)); //Declaring the new graph. Malloc a space in the heap to the graph struct and point to him
-  newGraph->Mark = (int*) malloc(sizeof(int) * n);  //Declare the size of the array of visited vertices
+  newGraph->Mark = (int*) calloc(0, sizeof(int) * n);  //Declare the size of the array of visited vertices
   newGraph->matrix = (int**) malloc(sizeof(int*) * n);  //Declare the size of the matrix of adjacency(A array that points to arrays)
 
   newGraph->numEdge = 0;  //Declare the start number of edges
   newGraph->numVertex = n; //Declare the number of verte=ices of the fraph
+
+  for(int i = 0; i < newGraph->numVertex; i++){
+    newGraph->matrix[i] = (int*) calloc(0, sizeof(int) * n);
+  }
 
   return newGraph;  //Return the created graph
 }
 
 //Func that return the first adjacent vertice of one given vertex
 int first(Graph* g, int v){
-  for(int i = 0; i < (sizeof(g->matrix[v])/sizeof(int)) -1 ; i++){  //For that iterates by the row of the given vertex
+  for(int i = 0; i < g->numVertex; i++){  //For that iterates by the row of the given vertex
     if(g->matrix[v][i] != 0){ //Check if the item is adjacent
       return i; //Return the adjacent item
     }
   }
-  return sizeof(g->matrix[v])/sizeof(int);  //If not have adjacents, return the number of vertices
+  return g->numVertex;  //If not have adjacents, return the number of vertices
 }
 
 //Func that return the next adjacent vertice of a given vertice, starting by an another adjacent given vertice
 int next(Graph* g, int v, int w){
-  for(int i = w + 1; i < (sizeof(g->matrix[v])/sizeof(int)) - 1; i++){  //For that iterates by the row of the given vertex, starting in the given adjacent
+  for(int i = w + 1; i < g->numVertex; i++){  //For that iterates by the row of the given vertex, starting in the given adjacent
     if(g->matrix[v][i] != 0){ //Check if the item is adjacent
       return i; //Return the adjacent vertice
     }
   }
-  return sizeof(g->matrix[v])/sizeof(int);  //If does not have adjacents, return the number of vertices
+  return g->numVertex;  //If does not have adjacents, return the number of vertices
 }
 
 //Func that set an edge between two vertices
@@ -72,38 +76,94 @@ int get_mark(Graph* g, int v){
   return g->Mark[v];
 }
 
-void DFS(Graph* g, int v){
-  set_mark(g, v, 1);
-  int w = first(g, v);
-  while(w < sizeof(g->matrix[v])/sizeof(int)){
-    if(get_mark(g, w) == 0){
-      DFS(g, w);
+//Func that does something in the start of the traverse  of one vertice
+void pre_visit(Graph* g, int v){
+  printf("Oi %d\n", v);
+}
+
+//Func that does something in the exit of the traverse of one vertice
+void pos_visit(Graph* g, int v){
+  printf("Tchau %d\n", v);
+}
+
+//Algorithm of DFS
+void DFS(Graph* g, int v){  //Receive the graph and the starter vertex as arguments
+  pre_visit(g, v);  //Calls the pre visit func
+  set_mark(g, v, 1);  //Mark the start vertice as visited
+  int w = first(g, v);  //Declare w as the firt adjacent vertex of v
+  while(w < g->numVertex){  //Loop that pass trought all the adjacent vertices of v
+    if(get_mark(g, w) == 0){  //Check if the vertices was already visited
+      DFS(g, w);  //If is a unvisited vertice, call the func again
     }
-    w = next(g, v, w);
+    w = next(g, v, w);  //Go to te next adjacent vertice
+  }
+  pos_visit(g, v);  //When exit of the vertice
+}
+
+//Algorithm of BFS
+void BFS(Graph* g, int start){  //Receive tthe graph and the starter vertex as arguments
+  queue<int> q; //Declaring the queue that bases the algorithm
+  q.push(start);  //Add the start vertice to the queue
+  set_mark(g, start, 1);  //Set the start vertice as visited
+
+  while(q.size() > 0){  //While that passes trought all connected vertices of the graph
+    int v = q.front();  //Declare the current vertice as the front of the queue
+    q.pop();  //Pop the front vertice of the queue
+    pre_visit(g, v);  //Calls the previsit func
+    int w = first(g, v);  //Declare the first adjacent vertice of the v vertex  
+
+    while(w < g->numVertex){  //Loop that goes trought all the adjacent vertex of v
+      if(get_mark(g, w) == 0){  //Check if it is a already visited vertice
+        set_mark(g, w, 1);  //Set as visited
+        q.push(w);  //Enqueue the vertice
+      }
+      w = next(g, v, w);  //Declare as the next adjacent vertex
+    }
+    pos_visit(g, v);  //Calls the pos visit func
   }
 }
-
-void BFS(Graph* g, int start){
-}
-
-
-
 
 //Func to traverse the graph with the DFS algorithm
-void graph_travers_Dfs(Graph* g){
-  for(int i = 0; i < sizeof(g->matrix[i])/sizeof(int); i++){
+void graph_travers_DFS(Graph* g){
+  for(int i = 0; i < g->numVertex; i++){  //For that declare all the vertices unvisited
     set_mark(g, i, 0);
   }
-  for(int i = 0; i < sizeof(g->matrix[i])/sizeof(int); i++){
+  for(int i = 0; i < g->numVertex; i++){  //For that pass trought all the vertices of the graph
     if(get_mark(g, i) == 0){
       DFS(g, i);
     }
   }
 }
 
+//Func that traverse the graph with the BFS algorithm
+void graph_travers_BFS(Graph* g){
+  for(int i = 0; i < g->numVertex; i++){  //For that declare all the vertices unvisited
+    set_mark(g, i, 0);
+  }
+  for(int i = 0; i < g->numVertex; i++){  //For that pass trought all the vertices of the graph
+    if(get_mark(g, i) == 0){
+      BFS(g, i);
+    }
+  }
+}
+
 int main(){
 
-  printf("Oi");
+
+  int numberVertex;
+  scanf("%d", &numberVertex);
+  Graph* newGraph = create_graph(numberVertex); 
+
+  int numberEdges;
+  scanf("%d" ,&numberEdges);
+  for(int i = 0; i < numberEdges; i++){
+    int vert1, vert2;
+    scanf("%d %d", &vert1, &vert2);
+    set_edge(newGraph, vert1, vert2, 1);
+  }
+
+  graph_travers_BFS(newGraph);
+
 
   return 0;
 }
