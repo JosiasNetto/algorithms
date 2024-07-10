1ebaf607bcc0b08ffc759092a2761112cd44d4ca
JosiasNetto <josiascaldasnetto08@gmail.com>
2024-07-10 20:46:11 -0300

    Finished List 5 Qb

--- listVjudge/list5/b-toposort.cpp
+++ listVjudge/list5/b-toposort.cpp
@@ -9,6 +9,7 @@ typedef struct graph{
     int numVert;
 
     vector<int> mark;
+    vector<int> inDegree;
     vector<list<int>*> matrix;
 }Graph;
 
@@ -19,6 +20,7 @@ Graph* create_graph(int n){
     newGraph->numVert = n;
 
     newGraph->mark.assign(n, UNVISITED);
+    newGraph->inDegree.assign(n, 0);
     for(int i = 0; i < n + 1; i++){
         newGraph->matrix.push_back(new list<int>);
     }
@@ -38,6 +40,7 @@ void set_edge(Graph* g, int v, int w){
     auto it = find(g->matrix[v]->begin(), g->matrix[v]->end(), w);
     if(it == g->matrix[v]->end()){
         g->matrix[v]->push_back(w);
+        g->numEdges++;
     }
 }
 
@@ -70,25 +73,49 @@ int no_points(Graph* g, int v){
 }
 
 void clear_list(Graph* g, int v){
+    for(int w : *g->matrix[v]){
+      g->inDegree[w]--;
+    }
+
+    g->numEdges = g->numEdges - g->matrix[v]->size();
     g->matrix[v]->clear();
 }
 
 void toposort(Graph* g){
     priority_queue<int, vector<int>, greater<int> > minHp;
     vector<int> results;
+    int cnt = 0;
+    int found = 0;
 
-    for(int i = 0; i < g->numVert; i++){
-        if(get_mark(g, i) == 0 && no_points(g, i) == 0){
-            minHp.push(i);
-        }
+    for(int j = 0; j < g->numVert; j++){
+      cnt = 0;
+      found = 0;
+      while(cnt < g->numVert && found == 0){
+          if(get_mark(g, cnt) == 0 && g->inDegree[cnt] == 0){
+              minHp.push(cnt);
+              set_mark(g, cnt, 1);
+              found = 1;
+          }
+          cnt++;
+      }
+
+      if(minHp.size() == 0 && g->numEdges > 0){
+        cout<< "Sandro fails.";
+        return;
+      }
+
+      while(minHp.size() > 0){
+          int v = minHp.top();
+          minHp.pop();
+          results.push_back(v + 1);
+          clear_list(g, v);
+
+      }
+    }
+    for(int i = 0; i < results.size(); i++){
+      cout<< results[i]<< " ";
     }
 
-    while(minHp.size() > 0){
-        int v = minHp.top();
-        minHp.pop();
-        results.push_back(v + 1);
-
-    }
 }
 
 int main(){
@@ -100,7 +127,10 @@ int main(){
     for(int i = 0; i < edges; i++){
         int v1, v2;
         cin>> v1>> v2;
-        set_edge(graph,v1 - 1, v2 - 1);
+        set_edge(graph,v1 -1 , v2 -1);
+        graph->inDegree[v2 - 1]++;
     }
+
+    toposort(graph);
     
 }
