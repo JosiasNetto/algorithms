5374bf9176a82c40382104a31bfeb54959360ce1
jhcn <jhcn@g2c41.cin.ufpe.br>
2024-07-10 16:44:17 -0300

    Continue b-toposort

--- listVjudge/list5/b-toposort.cpp
+++ listVjudge/list5/b-toposort.cpp
@@ -19,7 +19,7 @@ Graph* create_graph(int n){
     newGraph->numVert = n;
 
     newGraph->mark.assign(n, UNVISITED);
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n + 1; i++){
         newGraph->matrix.push_back(new list<int>);
     }
 
@@ -58,4 +58,49 @@ int next(Graph* g, int v, int w){
     else{
         return g->numVert;
     }
+}
+
+int no_points(Graph* g, int v){
+    for(int i = 0; i < g->numVert; i++){
+        if(find(g->matrix[i]->begin(), g->matrix[i]->end(), v) != g->matrix[i]->end()){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void clear_list(Graph* g, int v){
+    g->matrix[v]->clear();
+}
+
+void toposort(Graph* g){
+    priority_queue<int, vector<int>, greater<int> > minHp;
+    vector<int> results;
+
+    for(int i = 0; i < g->numVert; i++){
+        if(get_mark(g, i) == 0 && no_points(g, i) == 0){
+            minHp.push(i);
+        }
+    }
+
+    while(minHp.size() > 0){
+        int v = minHp.top();
+        minHp.pop();
+        results.push_back(v + 1);
+
+    }
+}
+
+int main(){
+    int vert, edges;
+    cin>> vert >> edges;
+
+    Graph* graph = create_graph(vert);
+
+    for(int i = 0; i < edges; i++){
+        int v1, v2;
+        cin>> v1>> v2;
+        set_edge(graph,v1 - 1, v2 - 1);
+    }
+    
 }
