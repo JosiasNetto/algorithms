8a3dd9c16f426929dfae8d6fc731a7eee9314a42
Josias Netto <josiascaldasnetto08@gmail.com>
2024-07-26 11:50:32 -0300

    Continue list 7 qa

--- listVjudge/list7/a-making-jumps.cpp
+++ listVjudge/list7/a-making-jumps.cpp
@@ -2,21 +2,42 @@
 
 using namespace std;
 
+bool jump_knight(int row, vector<vector<int> > board){
+  if(row == board.size()){
+    return true;
+  }
+  else{
+    for(int i = 0; i < 8; i++){
+
+    }
+  }
+
+}
+
 int main(){
   int rows, cnt;
   cin >> rows;
   cnt = 0;
 
 while(rows){
-    vector<vector<int> > board(rows);
+    int board[10][10];
     cnt++;
 
-    int skiped, valid;
+    int skiped, valid, cntValid;
+    cntValid = 0;
     for(int i = 0; i < rows; i++){
       cin >> skiped >> valid;
-      board[i].assign(skiped, -1);
-      for(int j = 0; j < valid; j++){
-        board[i].push_back(0);
+      for(int j = 0; j < 10; j++){
+        if(j < skiped){
+          board[i][j] = -1;
+        }
+        else if(cntValid < valid){
+          board[i][j] = 0;
+          cntValid++;
+        }
+        else{
+          board[i][j] = -1;
+        }
       }
     }
     cin >> rows;
