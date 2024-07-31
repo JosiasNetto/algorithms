e4a3ce1072e139457be68d827f6c724a8b447b8f
Josias Netto <josiascaldasnetto08@gmail.com>
2024-07-31 12:21:23 -0300

    List 8 qb

--- listVjudge/list8/b-frogs2.cpp
+++ listVjudge/list8/b-frogs2.cpp
@@ -3,8 +3,19 @@
 using namespace std;
 
 int jump_frog(int numStones, int maxDist,  int heightStones[]){
-  int distStones[numStones];
+  int position;
+  position = 0;
+  vector<int> distStones(numStones, INT_MAX);
+  distStones[0] = 0;
 
+  for(int i = 0; i < numStones; i++){
+    for(int j = 1; j < maxDist + 1; j++){
+      if(i + j < numStones){
+        distStones[i + j] = min(distStones[i + j], distStones[i] + abs(heightStones[i] - heightStones[i + j]));
+      }
+    }
+  }
+  return distStones[numStones - 1];
 }
 
 int main(){
@@ -17,6 +28,7 @@ int main(){
     cin >> height;
     heightStones[i] = height;
   }
+  cout<< jump_frog(numStones, maxDist, heightStones);
 
 
 }
